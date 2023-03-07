3ac57c1dd1caee083f55c1090129412a5459d104
Sabbadini10 <matiassabbadini24@gmail.com>
2023-03-07 18:06:11 -0300

    seccion custom-pipes terminada

--- src/app/shared/pipes/dark-mode.pipe.ts
+++ src/app/shared/pipes/dark-mode.pipe.ts
@@ -5,6 +5,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DarkModePipes implements PipeTransform {
   transform(value: boolean) {
-    return value ? 'bg-dark text-white' : 'bg-ligth text-dark';
+    return value ? 'bg-dark  text-white' : 'bg-ligth text-dark';
   }
 }
